3233f917d63d2bf02e8babf4856853f2c4a801dd
Kieran <kieranc.inbox@gmail.com>
2022-01-27 17:50:19 +1100

    Enemy Animations

---
+++ Assets/LC Game/Scripts/Enemy/BasicEnemyController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LCGame.Enemy
+{
+    public class BasicEnemyController : MonoBehaviour
+    {
+        
+    }
+}
